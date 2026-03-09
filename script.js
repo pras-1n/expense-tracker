6d3bae07a5a13ca231c3f50100a0582ec0f4371b
Prashant Guragai <prashantguragai1016@gmail.com>
2026-03-09 19:33:08 +0545

    local storage set up

--- script.js
+++ script.js
@@ -1,4 +1,6 @@
-let transactions = [];
+const savedTransactions = localStorage.getItem("myTransactions");
+
+let transactions = savedTransactions ? JSON.parse(savedTransactions) : [];
 
 const transactionForm = document.getElementById("transaction-form");
 transactionForm.addEventListener("submit", (event) => {
@@ -21,4 +23,28 @@ transactionForm.addEventListener("submit", (event) => {
   };
   transactions.push(transaction);
   transactionForm.reset();
+  updateUI();
+  localStorage.setItem("myTransactions", JSON.stringify(transactions));
 });
+
+const updateUI = () => {
+  document.getElementById("transaction-list").innerHTML = "";
+  transactions.forEach((transaction) => {
+    const transactionItem = document.createElement("li");
+    transactionItem.textContent = `${transaction.type} | ${transaction.name} - NRS ${transaction.amount} - ${transaction.date}`;
+    document.getElementById("transaction-list").appendChild(transactionItem);
+  });
+  const incomes = transactions
+    .filter((t) => t.type === "Income")
+    .reduce((sum, t) => sum + t.amount, 0);
+  const expenses = transactions
+    .filter((t) => t.type === "Expense")
+    .reduce((sum, t) => sum + t.amount, 0);
+  document.getElementById("income").textContent = `NRS ${incomes.toFixed(2)}`;
+  document.getElementById("expense").textContent = `NRS ${expenses.toFixed(2)}`;
+
+  const balance = incomes - expenses;
+  document.getElementById("balance").textContent = `NRS ${balance.toFixed(2)}`;
+};
+
+updateUI();
